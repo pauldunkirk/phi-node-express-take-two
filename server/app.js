00986a6ec0498ab54968bb1234ca1969629726c2
Paul Dunkirk <pauldunkirk@gmail.com>
2017-02-22 11:34:25 -0600

    serving static files

--- server/app.js
+++ server/app.js
@@ -1,1 +1,8 @@
 console.log('starting up the server< this is a lie for now');
+
+var express = require('express'); // called in express (node does this)
+var app = express(); // express now function - puts express into app
+
+app.use(express.static('server/public')); //runs every time there is a request from browser
+
+app.listen(5000); // this will work AFTER you npm start
--- server/public/client.js
+++ server/public/client.js
@@ -1,4 +1,4 @@
 console.log('sourced');
 $(document).ready(function(){
-  
+  console.log('jquery was sourced');
 })
