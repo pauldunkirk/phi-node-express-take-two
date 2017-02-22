9ff08fd3a99cbe42a1786da05a8ffd8030661ae6
Paul Dunkirk <pauldunkirk@gmail.com>
2017-02-22 17:02:55 -0600

    base mode

--- server/app.js
+++ server/app.js
@@ -2,35 +2,50 @@ console.log('starting up the server');
 
 var express = require('express'); // called in express (node does this)
 var app = express(); // express now function - puts express into app
+var bodyParser = require('body-parser');
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public')); //runs every time there is a request from browser
 
-var fishiesList = [{name: 'walleye'},{name: 'pike'}, {name: 'muskie'}]
+var fishiesList = [{
+    name: 'walleye'
+}, {
+    name: 'pike'
+}, {
+    name: 'muskie'
+}];
 
 
-app.get('/fish', function(req, res){ // express has already created req and res
-  console.log('req', req); //this shows up on Terminal - console.log from client in browser
-  res.send(fishiesList); // always need a response of some kind
+app.get('/fish', function(req, res) { // express has already created req and res
+    console.log('req', req); //this shows up on Terminal - console.log from client in browser
+    res.send(fishiesList); // always need a response of some kind
 });
 
-app.get('/fish/first', function(req, res){
-  res.send(fishiesList[0]);
+app.get('/fish/first', function(req, res) {
+    res.send(fishiesList[0]);
 });
 
-app.get('/fish/first/name', function(req, res){
-  res.send(fishiesList[0].name);
+app.get('/fish/first/name', function(req, res) {
+    res.send(fishiesList[0].name);
 });
 
-app.get('/fish/last', function(req, res){
-  res.send(fishiesList[fishiesList.length-1]);
+app.get('/fish/last', function(req, res) {
+    res.send(fishiesList[fishiesList.length - 1]);
 });
 
-app.get('/fish/last/name', function(req, res){
-  res.send(fishiesList[fishiesList.length-1].name);
+app.get('/fish/last/name', function(req, res) {
+    res.send(fishiesList[fishiesList.length - 1].name);
 });
 
-
+app.post('/fish/new', function(req, res){
+    var newFish = req.body;
+    if (req.body.name === '') {
+      res.status(400).send('please add a fish name before clicking');
+    } else {
+      fishiesList.push(newFish);
+      res.sendStatus(200);
+    }
+});
 
 
 app.listen(5000); // this will work AFTER you npm start
--- server/public/client.js
+++ server/public/client.js
@@ -1,29 +1,48 @@
 console.log('sourced');
-$(document).ready(function(){
-  console.log('jquery was sourced');
-  $.ajax({
-    type: 'GET',
-    url: '/fish',
-    success: function(response){
-      console.log('response', response);
-      for (var i = 0; i < response.length; i++) {
-        $('#fishTank').append('<li>' + response[i].name + '</li>');
-      } //end for
-      $('#firstFishy').text(response[0].name); 
-    } // end of success
-  });// end of AJAX request
-
-  $.ajax({
-    type: 'GET',
-    url: '/fish/first/name',
-    success: function(response){
-      console.log('response', response);
-      $('#firstFishy').text(response);
-    } // end of success
-  });// end of AJAX request
+$(document).ready(function() {
+    console.log('jquery was sourced');
+    getFishData();
+    function getFishData() {
+        $.ajax({
+            type: 'GET',
+            url: '/fish',
+            success: function(response) {
+                console.log('response', response);
+                $('#fishTank').empty();
+                for (var i = 0; i < response.length; i++) {
+                    $('#fishTank').append('<li>' + response[i].name + '</li>');
+                } //end for
+                $('#firstFishy').text(response[0].name);
+            } // end of success
+        }); // end of AJAX request
 
 
+        $.ajax({
+            type: 'GET',
+            url: '/fish/first/name',
+            success: function(response) {
+                console.log('response', response);
+                $('#firstFishy').text(response);
+            } // end of success
+        }); // end of AJAX request
+    }
 
-
-
+    $('#newFishButton').on('click', function() {
+        $('#error').empty();
+        var newFishObject = {};
+        newFishObject.name = $('#newFishName').val();
+        $.ajax({
+            type: 'POST',
+            url: '/fish/new',
+            data: newFishObject,
+            success: function(response) {
+              console.log(response);
+              getFishData();
+            }, //end success
+            error: function(error){
+        console.log('error', error);
+        $('#error').text('Please enter a fish name before clicking');
+      }
+        }); //end AJAX
+    }); // end on click
 }); // end doc ready
