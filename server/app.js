1e0f74b9c672a0f1fa6f3335e0641eed6c6ec917
Paul Dunkirk <pauldunkirk@gmail.com>
2017-02-22 15:22:36 -0600

    requests

--- server/app.js
+++ server/app.js
@@ -1,8 +1,36 @@
-console.log('starting up the server< this is a lie for now');
+console.log('starting up the server');
 
 var express = require('express'); // called in express (node does this)
 var app = express(); // express now function - puts express into app
 
+
 app.use(express.static('server/public')); //runs every time there is a request from browser
+
+var fishiesList = [{name: 'walleye'},{name: 'pike'}, {name: 'muskie'}]
+
+
+app.get('/fish', function(req, res){ // express has already created req and res
+  console.log('req', req); //this shows up on Terminal - console.log from client in browser
+  res.send(fishiesList); // always need a response of some kind
+});
+
+app.get('/fish/first', function(req, res){
+  res.send(fishiesList[0]);
+});
+
+app.get('/fish/first/name', function(req, res){
+  res.send(fishiesList[0].name);
+});
+
+app.get('/fish/last', function(req, res){
+  res.send(fishiesList[fishiesList.length-1]);
+});
+
+app.get('/fish/last/name', function(req, res){
+  res.send(fishiesList[fishiesList.length-1].name);
+});
+
+
+
 
 app.listen(5000); // this will work AFTER you npm start
--- server/public/client.js
+++ server/public/client.js
@@ -1,4 +1,29 @@
 console.log('sourced');
 $(document).ready(function(){
   console.log('jquery was sourced');
-})
+  $.ajax({
+    type: 'GET',
+    url: '/fish',
+    success: function(response){
+      console.log('response', response);
+      for (var i = 0; i < response.length; i++) {
+        $('#fishTank').append('<li>' + response[i].name + '</li>');
+      } //end for
+      $('#firstFishy').text(response[0].name); 
+    } // end of success
+  });// end of AJAX request
+
+  $.ajax({
+    type: 'GET',
+    url: '/fish/first/name',
+    success: function(response){
+      console.log('response', response);
+      $('#firstFishy').text(response);
+    } // end of success
+  });// end of AJAX request
+
+
+
+
+
+}); // end doc ready
